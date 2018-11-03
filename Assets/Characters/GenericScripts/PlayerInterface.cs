adee4a47c607bd6756a3f93ed94e1bccb52638a9
avokadoen <akselhallajs@gmail.com>
2018-11-03 11:54:35 +0100

    pushing as is working on camera system

--- Assets/Characters/GenericScripts/PlayerInterface.cs
+++ Assets/Characters/GenericScripts/PlayerInterface.cs
@@ -191,4 +191,9 @@ public class PlayerInterface : NetworkBehaviour
             }
         }
     }
+
+    public bool IsLocalPlayer()
+    {
+        return isLocalPlayer;
+    }
 }
---
+++ Assets/Scripts/FindLocalPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindLocalPlayer : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(var player in players)
+        {
+            
+        }
+    }
+}
